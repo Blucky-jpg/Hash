592683a5ec05b349130abee760326c0ba486da25
Blucky7 <116442979+Blucky-jpg@users.noreply.github.com>
2024-09-20 22:16:03 +0200

    Update md5.c

--- md5.c
+++ md5.c
@@ -1,305 +1,223 @@
+/*
+ * Derived from the RSA Data Security, Inc. MD5 Message-Digest Algorithm
+ * and modified slightly to be functionally identical but condensed into control structures.
+ */
+
 #include "md5.h"
 
-char * md5(Blocks *blocks) {
-    word *T = generateT();
+/*
+ * Constants defined by the MD5 algorithm
+ */
+#define A 0x67452301
+#define B 0xefcdab89
+#define C 0x98badcfe
+#define D 0x10325476
 
-    int numBlocks = blocks->numBlocks;
-    word **M = blocks->words;
+static uint32_t S[] = {7, 12, 17, 22, 7, 12, 17, 22, 7, 12, 17, 22, 7, 12, 17, 22,
+                       5,  9, 14, 20, 5,  9, 14, 20, 5,  9, 14, 20, 5,  9, 14, 20,
+                       4, 11, 16, 23, 4, 11, 16, 23, 4, 11, 16, 23, 4, 11, 16, 23,
+                       6, 10, 15, 21, 6, 10, 15, 21, 6, 10, 15, 21, 6, 10, 15, 21};
 
-    // initialise MD buffer
-    // (low-byte order)
-    word A = 0x67452301;
-    word B = 0xefcdab89;
-    word C = 0x98badcfe;
-    word D = 0x10325476;
+static uint32_t K[] = {0xd76aa478, 0xe8c7b756, 0x242070db, 0xc1bdceee,
+                       0xf57c0faf, 0x4787c62a, 0xa8304613, 0xfd469501,
+                       0x698098d8, 0x8b44f7af, 0xffff5bb1, 0x895cd7be,
+                       0x6b901122, 0xfd987193, 0xa679438e, 0x49b40821,
+                       0xf61e2562, 0xc040b340, 0x265e5a51, 0xe9b6c7aa,
+                       0xd62f105d, 0x02441453, 0xd8a1e681, 0xe7d3fbc8,
+                       0x21e1cde6, 0xc33707d6, 0xf4d50d87, 0x455a14ed,
+                       0xa9e3e905, 0xfcefa3f8, 0x676f02d9, 0x8d2a4c8a,
+                       0xfffa3942, 0x8771f681, 0x6d9d6122, 0xfde5380c,
+                       0xa4beea44, 0x4bdecfa9, 0xf6bb4b60, 0xbebfbc70,
+                       0x289b7ec6, 0xeaa127fa, 0xd4ef3085, 0x04881d05,
+                       0xd9d4d039, 0xe6db99e5, 0x1fa27cf8, 0xc4ac5665,
+                       0xf4292244, 0x432aff97, 0xab9423a7, 0xfc93a039,
+                       0x655b59c3, 0x8f0ccc92, 0xffeff47d, 0x85845dd1,
+                       0x6fa87e4f, 0xfe2ce6e0, 0xa3014314, 0x4e0811a1,
+                       0xf7537e82, 0xbd3af235, 0x2ad7d2bb, 0xeb86d391};
 
-    word AA, BB, CC, DD;
+/*
+ * Padding used to make the size (in bits) of the input congruent to 448 mod 512
+ */
+static uint8_t PADDING[] = {0x80, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+                            0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+                            0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+                            0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+                            0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+                            0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+                            0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+                            0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
 
-    for (int i = 0; i < numBlocks; i++) {
-        word *X = M[i];
+/*
+ * Bit-manipulation functions defined by the MD5 algorithm
+ */
+#define F(X, Y, Z) ((X & Y) | (~X & Z))
+#define G(X, Y, Z) ((X & Z) | (Y & ~Z))
+#define H(X, Y, Z) (X ^ Y ^ Z)
+#define I(X, Y, Z) (Y ^ (X | ~Z))
 
-        AA = A;
-        BB = B;
-        CC = C;
-        DD = D;
-
-        // Round 1
-        R1_OP(A, B, C, D, X[0], 7, T[1]);
-        R1_OP(D, A, B, C, X[1], 12, T[2]);
-        R1_OP(C, D, A, B, X[2], 17, T[3]);
-        R1_OP(B, C, D, A, X[3], 22, T[4]);
-
-        R1_OP(A, B, C, D, X[4], 7, T[5]);
-        R1_OP(D, A, B, C, X[5], 12, T[6]);
-        R1_OP(C, D, A, B, X[6], 17, T[7]);
-        R1_OP(B, C, D, A, X[7], 22, T[8]);
-
-        R1_OP(A, B, C, D, X[8], 7, T[9]);
-        R1_OP(D, A, B, C, X[9], 12, T[10]);
-        R1_OP(C, D, A, B, X[10], 17, T[11]);
-        R1_OP(B, C, D, A, X[11], 22, T[12]);
-
-        R1_OP(A, B, C, D, X[12], 7, T[13]);
-        R1_OP(D, A, B, C, X[13], 12, T[14]);
-        R1_OP(C, D, A, B, X[14], 17, T[15]);
-        R1_OP(B, C, D, A, X[15], 22, T[16]);
-
-        // Round 2
-        R2_OP(A, B, C, D, X[1], 5, T[17]);
-        R2_OP(D, A, B, C, X[6], 9, T[18]);
-        R2_OP(C, D, A, B, X[11], 14, T[19]);
-        R2_OP(B, C, D, A, X[0], 20, T[20]);
-
-        R2_OP(A, B, C, D, X[5], 5, T[21]);
-        R2_OP(D, A, B, C, X[10], 9, T[22]);
-        R2_OP(C, D, A, B, X[15], 14, T[23]);
-        R2_OP(B, C, D, A, X[4], 20, T[24]);
-
-        R2_OP(A, B, C, D, X[9], 5, T[25]);
-        R2_OP(D, A, B, C, X[14], 9, T[26]);
-        R2_OP(C, D, A, B, X[3], 14, T[27]);
-        R2_OP(B, C, D, A, X[8], 20, T[28]);
-
-        R2_OP(A, B, C, D, X[13], 5, T[29]);
-        R2_OP(D, A, B, C, X[2], 9, T[30]);
-        R2_OP(C, D, A, B, X[7], 14, T[31]);
-        R2_OP(B, C, D, A, X[12], 20, T[32]);
-
-        // Round 3
-        R3_OP(A, B, C, D, X[5], 4, T[33]);
-        R3_OP(D, A, B, C, X[8], 11, T[34]);
-        R3_OP(C, D, A, B, X[11], 16, T[35]);
-        R3_OP(B, C, D, A, X[14], 23, T[36]);
-
-        R3_OP(A, B, C, D, X[1], 4, T[37]);
-        R3_OP(D, A, B, C, X[4], 11, T[38]);
-        R3_OP(C, D, A, B, X[7], 16, T[39]);
-        R3_OP(B, C, D, A, X[10], 23, T[40]);
-
-        R3_OP(A, B, C, D, X[13], 4, T[41]);
-        R3_OP(D, A, B, C, X[0], 11, T[42]);
-        R3_OP(C, D, A, B, X[3], 16, T[43]);
-        R3_OP(B, C, D, A, X[6], 23, T[44]);
-
-        R3_OP(A, B, C, D, X[9], 4, T[45]);
-        R3_OP(D, A, B, C, X[12], 11, T[46]);
-        R3_OP(C, D, A, B, X[15], 16, T[47]);
-        R3_OP(B, C, D, A, X[2], 23, T[48]);
-
-        // Round 4
-        R4_OP(A, B, C, D, X[0], 6, T[49]);
-        R4_OP(D, A, B, C, X[7], 10, T[50]);
-        R4_OP(C, D, A, B, X[14], 15, T[51]);
-        R4_OP(B, C, D, A, X[5], 21, T[52]);
-
-        R4_OP(A, B, C, D, X[12], 6, T[53]);
-        R4_OP(D, A, B, C, X[3], 10, T[54]);
-        R4_OP(C, D, A, B, X[10], 15, T[55]);
-        R4_OP(B, C, D, A, X[1], 21, T[56]);
-
-        R4_OP(A, B, C, D, X[8], 6, T[57]);
-        R4_OP(D, A, B, C, X[15], 10, T[58]);
-        R4_OP(C, D, A, B, X[6], 15, T[59]);
-        R4_OP(B, C, D, A, X[13], 21, T[60]);
-
-        R4_OP(A, B, C, D, X[4], 6, T[61]);
-        R4_OP(D, A, B, C, X[11], 10, T[62]);
-        R4_OP(C, D, A, B, X[2], 15, T[63]);
-        R4_OP(B, C, D, A, X[9], 21, T[64]);
-
-        // add AA, BB, CC, and DD to MD buffer
-        A += AA;
-        B += BB;
-        C += CC;
-        D += DD;
-    }
-
-    // print ABCD in low-byte order
-    word state[4] = {A, B, C, D};
-    word w;
-    char *result = (char *)malloc(33 * sizeof(char));
-    for (int i = 0; i < 4; i++) {
-        w = state[i];
-        
-        // format string into a char array: https://stackoverflow.com/a/39495281
-        sprintf(result + (i * 8),
-                "%02x%02x%02x%02x",
-                w & 0xff,
-                (w >> 8) & 0xff,
-                (w >> 16) & 0xff,
-                (w >> 24) & 0xff
-            );
-    }
-    // append null terminator
-    result[32] = '\0';
-
-    // free all dynamically allocated memory
-    for (int i = 0; i < numBlocks; i++) {
-        free(M[i]);
-    }
-
-    // free block memory since it's no longer needed
-    free(M);
-    free(blocks);
-
-    return result;
+/*
+ * Rotates a 32-bit word left by n bits
+ */
+uint32_t rotateLeft(uint32_t x, uint32_t n){
+    return (x << n) | (x >> (32 - n));
 }
 
-// generate the 64 element array T, as per the RFC
-word * generateT() {
-    // use static so T can be used when out of scope
-    static word T[65];
 
-    for (int i = 1; i <= 64; i++) {
-        T[i] = (word)(T_MULTIPLIER * fabs(sin(i)));
-    }
+/*
+ * Initialize a context
+ */
+void md5Init(MD5Context *ctx){
+    ctx->size = (uint64_t)0;
 
-    return T;
+    ctx->buffer[0] = (uint32_t)A;
+    ctx->buffer[1] = (uint32_t)B;
+    ctx->buffer[2] = (uint32_t)C;
+    ctx->buffer[3] = (uint32_t)D;
 }
 
-struct Blocks * makeBlocks(ubyte *bytes, int length) {
-    int p;
-    int shiftPlaces;
-    word *currBlock;
-    ull paddingBytes;
-    ull totalBytes;
-    ull totalBits;
+/*
+ * Add some amount of input to the context
+ *
+ * If the input fills out a block of 512 bits, apply the algorithm (md5Step)
+ * and save the result in the buffer. Also updates the overall size.
+ */
+void md5Update(MD5Context *ctx, uint8_t *input_buffer, size_t input_len){
+    uint32_t input[16];
+    unsigned int offset = ctx->size % 64;
+    ctx->size += (uint64_t)input_len;
 
-    // copy input bytes to leave input array unchanged,
-    // leaving some extra space for any amount of padding needed at the end
-    ubyte buffer[length + 72];
-    for (int i = 0; i < length; i++) {
-        buffer[i] = bytes[i];
-    }
+    // Copy each byte in input_buffer into the next space in our context input
+    for(unsigned int i = 0; i < input_len; ++i){
+        ctx->input[offset++] = (uint8_t)*(input_buffer + i);
 
-    // compute exact number of padding bytes needed
-    paddingBytes = 65 - ((length + 8) % 64 + 1);
-    // total number of bytes needed for all blocks (a multiple of 64)
-    // (file bytes + padding bytes + input length bytes)
-    totalBytes = length + paddingBytes + 8;
-    totalBits = length * 8;
-    
-    // start padding with a 1
-    p = length;
-    buffer[p++] = FIRST_PADDING_BYTE;
-
-    // rest of the padding is 0s
-    for (; p < totalBytes - 8; p++) {
-        buffer[p] = 0;
-    }
-
-    // append 8 bytes describing input length (least significant byte first)
-    shiftPlaces = 0;
-    for (; p < totalBytes; p++) {
-        buffer[p] = (totalBits >> shiftPlaces) & 0xffff;
-        shiftPlaces += 8;
-    }
-
-    // compute the number of blocks needed to store all of the buffer data
-    int numBlocks = totalBytes / 64;
-
-    // allocate the memory needed for the 2D array M
-    // https://stackoverflow.com/a/14088911
-    word **M = (word **)malloc(numBlocks * sizeof(word *));
-
-    for (int i = 0; i < numBlocks; i++) {
-        M[i] = (int *)malloc(16 * sizeof(word));
-    }
-
-    // initialise blocks struct containing the 2D words array and number of blocks
-    Blocks *blocks = (Blocks *)malloc(sizeof(Blocks));
-    blocks->words = M;
-    blocks->numBlocks = numBlocks;
-
-    // now convert the array of bytes into a series of blocks,
-    // each containing 16x 32 bit words (512 bits total)
-    p = 0;
-    for (int i = 0; i < numBlocks; i++) {
-        currBlock = M[i];
-
-        for (int j = 0; j < 16; j++) {
-            // combine 4 bytes into a word (little-endian, as per the RFC)
-            // and write the word to the block at the next index
-            currBlock[j] = buffer[p++] | (buffer[p++] << 8) | (buffer[p++] << 16) | (buffer[p++] << 24);
+        // If we've filled our context input, copy it into our local array input
+        // then reset the offset to 0 and fill in a new buffer.
+        // Every time we fill out a chunk, we run it through the algorithm
+        // to enable some back and forth between cpu and i/o
+        if(offset % 64 == 0){
+            for(unsigned int j = 0; j < 16; ++j){
+                // Convert to little-endian
+                // The local variable `input` our 512-bit chunk separated into 32-bit words
+                // we can use in calculations
+                input[j] = (uint32_t)(ctx->input[(j * 4) + 3]) << 24 |
+                           (uint32_t)(ctx->input[(j * 4) + 2]) << 16 |
+                           (uint32_t)(ctx->input[(j * 4) + 1]) <<  8 |
+                           (uint32_t)(ctx->input[(j * 4)]);
+            }
+            md5Step(ctx->buffer, input);
+            offset = 0;
         }
     }
-
-    // return pointer to blocks
-    return blocks;
 }
 
-struct Blocks * readFileAsBlocks(char *filePath) {
-    ull fileLength;
-    FILE *filePtr;
+/*
+ * Pad the current input to get to 448 bytes, append the size in bits to the very end,
+ * and save the result of the final iteration into digest.
+ */
+void md5Finalize(MD5Context *ctx){
+    uint32_t input[16];
+    unsigned int offset = ctx->size % 64;
+    unsigned int padding_length = offset < 56 ? 56 - offset : (56 + 64) - offset;
 
-    filePtr = fopen(filePath, "rb");
+    // Fill in the padding and undo the changes to size that resulted from the update
+    md5Update(ctx, PADDING, padding_length);
+    ctx->size -= (uint64_t)padding_length;
 
-    if (!filePtr) {
-        printf("File not found: %s\n", filePath);
-        exit(1);
+    // Do a final update (internal to this function)
+    // Last two 32-bit words are the two halves of the size (converted from bytes to bits)
+    for(unsigned int j = 0; j < 14; ++j){
+        input[j] = (uint32_t)(ctx->input[(j * 4) + 3]) << 24 |
+                   (uint32_t)(ctx->input[(j * 4) + 2]) << 16 |
+                   (uint32_t)(ctx->input[(j * 4) + 1]) <<  8 |
+                   (uint32_t)(ctx->input[(j * 4)]);
     }
+    input[14] = (uint32_t)(ctx->size * 8);
+    input[15] = (uint32_t)((ctx->size * 8) >> 32);
 
-    // reading binary files: https://stackoverflow.com/a/22059317
-    // seek to end of the file to find it's length
-    fseek(filePtr, 0, SEEK_END);
-    fileLength = ftell(filePtr);
-    rewind(filePtr);
+    md5Step(ctx->buffer, input);
 
-    // read the entire file into a byte buffer
-    ubyte *buffer = (ubyte *)malloc(fileLength* sizeof(ubyte));
-    fread(buffer, fileLength, 1, filePtr);
-
-    // convert byte array to blocks
-    Blocks *blocks = makeBlocks(buffer, fileLength);
-
-    // close file
-    fclose(filePtr);
-
-    // free buffer memory
-    free(buffer);
-
-    // return pointer to blocks
-    return blocks;
-}
-
-void printWordBits(word w) {
-    for (int i = 0; i < 32; i++) {
-        // next 1 or 0
-        word bit = (w & 0x80000000) != 0;
-        // print bit
-        printf("%" PRIu32, bit);
-        // shift w along by 1 so next bit is ready to be printed
-        w = w << 1;
+    // Move the result into digest (convert from little-endian)
+    for(unsigned int i = 0; i < 4; ++i){
+        ctx->digest[(i * 4) + 0] = (uint8_t)((ctx->buffer[i] & 0x000000FF));
+        ctx->digest[(i * 4) + 1] = (uint8_t)((ctx->buffer[i] & 0x0000FF00) >>  8);
+        ctx->digest[(i * 4) + 2] = (uint8_t)((ctx->buffer[i] & 0x00FF0000) >> 16);
+        ctx->digest[(i * 4) + 3] = (uint8_t)((ctx->buffer[i] & 0xFF000000) >> 24);
     }
-
-    printf("\n");
 }
 
-void printBlocks(struct Blocks *M) {
-    word **words = M->words;
-    int numBlocks = M->numBlocks;
+/*
+ * Step on 512 bits of input with the main MD5 algorithm.
+ */
+void md5Step(uint32_t *buffer, uint32_t *input){
+    uint32_t AA = buffer[0];
+    uint32_t BB = buffer[1];
+    uint32_t CC = buffer[2];
+    uint32_t DD = buffer[3];
 
-    printf("\n");
-    for (int i = 0; i < numBlocks; i++) {
-        printf("BLOCK: %d\n", i);
+    uint32_t E;
 
-        // print bits of each 32 bit word in turn
-        for (int j = 0; j < 16; j++) {
-            printWordBits(words[i][j]);
+    unsigned int j;
+
+    for(unsigned int i = 0; i < 64; ++i){
+        switch(i / 16){
+            case 0:
+                E = F(BB, CC, DD);
+                j = i;
+                break;
+            case 1:
+                E = G(BB, CC, DD);
+                j = ((i * 5) + 1) % 16;
+                break;
+            case 2:
+                E = H(BB, CC, DD);
+                j = ((i * 3) + 5) % 16;
+                break;
+            default:
+                E = I(BB, CC, DD);
+                j = (i * 7) % 16;
+                break;
         }
 
-        printf("\n");
+        uint32_t temp = DD;
+        DD = CC;
+        CC = BB;
+        BB = BB + rotateLeft(AA + E + K[i] + input[j], S[i]);
+        AA = temp;
     }
-    printf("\n");
+
+    buffer[0] += AA;
+    buffer[1] += BB;
+    buffer[2] += CC;
+    buffer[3] += DD;
 }
 
-bool isHashEqual(char *hashA, char *hashB) {
-    // compare hash values character by character
-    for (int i = 0; i < 32; i++) {
-        if (hashA[i] != hashB[i]) {
-            return false;
-        }
+/*
+ * Functions that run the algorithm on the provided input and put the digest into result.
+ * result should be able to store 16 bytes.
+ */
+void md5String(char *input, uint8_t *result){
+    MD5Context ctx;
+    md5Init(&ctx);
+    md5Update(&ctx, (uint8_t *)input, strlen(input));
+    md5Finalize(&ctx);
+
+    memcpy(result, ctx.digest, 16);
+}
+
+void md5File(FILE *file, uint8_t *result){
+    char *input_buffer = malloc(1024);
+    size_t input_size = 0;
+
+    MD5Context ctx;
+    md5Init(&ctx);
+
+    while((input_size = fread(input_buffer, 1, 1024, file)) > 0){
+        md5Update(&ctx, (uint8_t *)input_buffer, input_size);
     }
 
-    return true;
+    md5Finalize(&ctx);
+
+    free(input_buffer);
+
+    memcpy(result, ctx.digest, 16);
 }
